edb43ff089bb9f30c645d69317ce5219875ef2a2
nimbl <stashrustalev88@gmail.com>
2025-02-08 10:00:49 +0300

    fix_main

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import logging
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 bot = telebot.TeleBot("7512750009:AAHYQt5qndrJLjeUEHcl2IRwwDWakmgcm-A", parse_mode=None)
-admin = []
-visitor = []
+admins = []
+visitors = []
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-    if message.chat.id == admin:
+    if message.chat.id in admins:
         bot.send_message(message.chat.id, "/new_text - создать новое сообщение для рассылки \n"
                                           "/new_link - создать ссылку для рассылки \n"
                                           "/send - для отправки рассылки")
+    else:
+        bot.send_message(message.chat.id, "Привет!")
+
 
 @bot.message_handler(commands=['new_text'])
 def new_text(message):
@@ -25,5 +28,7 @@ def new_link(message):
 @bot.message_handler(commands=['send'])
 def send(message):
     bot.send_message(message.chat.id, "Рассылка успешно отправлена.")
+
+
 
 bot.infinity_polling()
